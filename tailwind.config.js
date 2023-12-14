14212428294833faba606f4414fb8ad7ac428190
rshaikh95 <itsrahil@live.com>
2023-12-14 01:45:24 +0000

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [/home/rahil_shaikh/flask_e2e_project/templates/home.html],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
